4d0faf9a563649bce1c76f97b6809bdda9767e7b
Khanh Tran <katetrannbkhanh@gmail.com>
2022-10-05 13:10:49 -0700

    commit tree e9ab23b373bc36e5095820f57f9da246d7b8a1bb

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(collection){
+  const res = collection.find(years => years.result === 'W');
+  if (res){
+    return res.year;
+  } else{
+    return undefined;
+  }
+}
